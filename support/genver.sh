3f5400c8017c40bcbde7c930d780925925b464f9
Shaun McDowell <shaunjmcdowell@gmail.com>
2017-09-12 16:04:59 -0400

    removed server, tests, trdump, gznbd, coverity from tree and build

--- support/genver.sh
+++ support/genver.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-GITDESC=$(git describe --dirty|sed -e 's/nbd-//' 2>/dev/null)
+GITDESC=$(git describe --dirty|sed -e 's/cbd-client-//' 2>/dev/null)
 
 if [ -z "$GITDESC" ]; then
 	GITDESC="0.unknown"
